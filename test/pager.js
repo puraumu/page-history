66cf80835732f9443576c64708d1940c01d919d2
Puraumu <puraumu@MacPro.local>
2012-08-07 09:21:57 +0900

    testing next, back, now

--- test/pager.js
+++ test/pager.js
@@ -1,4 +1,6 @@
-var Pager = require('../')
+var assert = require('assert')
+  , should = require('should')
+  , Pager = require('../')
   , pager = new Pager()
   , page = ['foo', 'bar', 'baz', 'hoge', 'FOO', 'BAR', 'HOGE']
 
@@ -19,29 +21,50 @@ describe('Pager', function(){
     it('should move to specified page', function(){
       pager.move(2).cwp.should.eql(2)
     })
+    it('should move to the page relatively', function(){
+      pager.move(2, true).cwp.should.eql(4)
+    })
+    it('should not be smaller than 0', function(){
+      pager.move(0).move(-1, true).cwp.should.eql(0)
+    })
+    it('should nto be bigger than Pager.len', function(){
+      pager.move(pager.len - 1).move(1, true).cwp.should.eql(pager.len - 1)
+    })
   })
 
   describe('.next()', function(){
     it('should return the next item', function(){
-      pager.next().should.eql('hoge')
+      pager.move(2).next().should.eql('hoge')
+    })
+    it('should stop when cwp is end of the page', function(){
+      should.strictEqual(pager.move(pager.len - 1).next(), undefined)
+      pager.cwp.should.eql(pager.len - 1)
     })
   })
 
   describe('.back()', function(){
     it('should return the previous item', function(){
-      pager.back().should.eql('baz')
+      pager.move(3).back().should.eql('baz')
+    })
+    it('should stop when cwp is top of the page', function(){
+      should.strictEqual(pager.move(0).back(), undefined)
+      pager.cwp.should.eql(0)
     })
   })
 
   describe('.now()', function(){
     it('should return current page', function(){
-      pager.now().should.eql('baz')
+      pager.move(1).now().should.eql('bar')
     })
   })
 
   describe('.action()', function(){
-    it('should', function(){
+    it('should register an action', function(){
       pager.action('next')._action.should.eql('next')
+    })
+    it('should not invoke noop', function(){
+      // => events
+
     })
   })
 
