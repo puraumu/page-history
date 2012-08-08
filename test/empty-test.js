8a78f9c68ea269d0d2f06759650a0c6f9c529f76
Puraumu <puraumu@MacPro.local>
2012-08-08 09:45:59 +0900

    empty test

---
+++ test/empty-test.js
@@ -0,0 +1,112 @@
+var assert = require('assert')
+  , should = require('should')
+  , Pager = require('../')
+  , pager = new Pager()
+  , page = ['foo', 'bar', 'baz', 'hoge', 'FOO', 'BAR', 'HOGE']
+
+describe('When Pager is empty', function(){
+
+  describe('new', function(){
+    it('should be initialized', function(){
+      pager = new Pager();
+      pager.page.should.be.empty;
+    })
+  })
+
+  describe('.move()', function(){
+    it('should not move and no event', function(done){
+      pager.once('top', function(d, i){
+        should.strictEqual(d, undefined)
+        i.should.eql(0)
+        true.should.be.false
+      })
+      setTimeout(function(){
+        done()
+      }, 20)
+      pager.move(0)
+      pager.cwp.should.eql(0)
+    })
+    it('should not move and no event', function(done){
+      pager.once('end', function(d, i){
+        true.should.be.false
+      })
+      setTimeout(function(){
+        done()
+      }, 20)
+      pager.move(1)
+      pager.cwp.should.eql(0)
+    })
+  })
+
+  describe('.next()', function(){
+    it('should not move and no event', function(done){
+      pager.once('move', function(d, i){
+        true.should.be.false
+      })
+      setTimeout(function(){
+        done()
+      }, 20)
+      pager.next()
+      pager.cwp.should.eql(0)
+    })
+  })
+
+  describe('.back() to the previous item.', function(){
+    it('should not move and no event', function(done){
+      pager.once('move', function(d, i){
+        true.should.be.false
+      })
+      setTimeout(function(){
+        done()
+      }, 20)
+      pager.back()
+      pager.cwp.should.eql(0)
+    })
+  })
+
+  describe('.now()', function(){
+    it('should return undefined', function(){
+      should.strictEqual(pager.now(), undefined)
+    })
+  })
+
+  describe('.index()', function(){
+    it('should return -1', function(){
+      pager.index(0).should.eql(-1)
+    })
+  })
+
+  describe('.push()', function(){
+  })
+
+  describe('.remove()', function(){
+    it('should do nothing or return self', function(){
+      pager.remove(0).should.eql(pager)
+      pager.len.should.eql(0)
+      pager.page.should.be.empty
+    })
+  })
+
+  describe('.add()', function(){
+  })
+
+  describe('.set()', function(){
+  })
+
+  describe('.get()', function(){
+  })
+
+  describe('.search()', function(){
+    it('should return []', function(){
+      pager.search(0).should.eql([])
+    })
+  })
+
+  describe('.export()', function(){
+    it('should return []', function(){
+      pager.export().should.eql([])
+    })
+  })
+
+})
+
