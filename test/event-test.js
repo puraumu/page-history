63cae9b74396d7ae0236ae079322f9422c4aa7cc
Puraumu <puraumu@MacPro.local>
2012-08-10 17:31:59 +0900

    change events (move, top, end) => (back, next)

--- test/event-test.js
+++ test/event-test.js
@@ -13,76 +13,37 @@ describe('Pager Events', function(){
     })
   })
 
-  describe('`move`', function(){
+  describe('`next`', function(){
     it('should return current page #1', function(done){
-      pager.once('move', function(d, i){
+      pager.once('next', function(d, i){
         d.should.eql('FOO')
         i.should.eql(4)
         done()
       })
       pager.move(4)
     })
-    it('should not emit `move` event when page sieze over', function(done){
-      pager.on('move', function(d, i){
+    it('should emit `next` event when page sieze over', function(done){
+      pager.once('next', function(d, i){
         d.should.eql('HOGE')
         i.should.eql(6)
-        false.should.be.true
+        done()
       })
       pager.move(456632)
-      setTimeout(function(){
-        pager.removeAllListeners('move')
-        done()
-      }, 50)
-    })
-    it('should return current page #2', function(done){
-      pager.move(4)
-      pager.once('move', function(d, i){
-        d.should.eql('hoge')
-        i.should.eql(3)
-        done()
-      })
-      pager.back()
     })
     it('should return current page #3', function(done){
-      pager.once('move', function(d, i){
-        d.should.eql('FOO')
-        i.should.eql(4)
+      pager.move(0)
+      pager.once('next', function(d, i){
+        d.should.eql('bar')
+        i.should.eql(1)
         done()
       })
       pager.next()
     })
   })
 
-  describe('`end`', function(){
-    it('should return the end of page #1', function(done){
-      pager.once('end', function(d, i){
-        d.should.eql('HOGE')
-        i.should.eql(6)
-        done()
-      })
-      pager.move(456632)
-    })
-    it('should return the end of page #2', function(done){
-      pager.once('end', function(d, i){
-        d.should.eql('HOGE')
-        i.should.eql(6)
-        done()
-      })
-      pager.move(pager.len - 1)
-    })
-    it('should return the end of page #3', function(done){
-      pager.once('end', function(d, i){
-        d.should.eql('HOGE')
-        i.should.eql(6)
-        done()
-      })
-      pager.next()
-    })
-  })
-
-  describe('`top`', function(){
-    it('should return the top of page #1', function(done){
-      pager.once('top', function(d, i){
+  describe('`back`', function(){
+    it('return current page #1', function(done){
+      pager.once('back', function(d, i){
         d.should.eql('foo')
         i.should.eql(0)
         done()
@@ -90,15 +51,16 @@ describe('Pager Events', function(){
       pager.move(-54)
     })
     it('should return the top of page #2', function(done){
-      pager.once('top', function(d, i){
-        d.should.eql('foo')
-        i.should.eql(0)
+      pager.move(3)
+      pager.once('back', function(d, i){
+        d.should.eql('bar')
+        i.should.eql(1)
         done()
       })
-      pager.move(0)
+      pager.move(1)
     })
     it('should return the top of page #3', function(done){
-      pager.once('top', function(d, i){
+      pager.once('back', function(d, i){
         d.should.eql('foo')
         i.should.eql(0)
         done()
